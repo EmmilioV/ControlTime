60edc7e3dee53f1f76f900b3ffdce88e3840f9e5
PABLO EMILIO VILLA PEREZ <pablovilla266477@correo.itm.edu.co>
2021-09-02 11:50:01 -0500

    Entity Created

---
+++ controltime.Functions/Entities/EmployeeRecordEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace controltime.Functions.Entities
+{
+    class EmployeeRecordEntity : TableEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
