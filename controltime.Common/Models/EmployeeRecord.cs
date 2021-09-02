785106e828015b57ca691397214705a9ed9a7577
PABLO EMILIO VILLA PEREZ <pablovilla266477@correo.itm.edu.co>
2021-09-02 06:32:27 -0500

    Models and Responses created

---
+++ controltime.Common/Models/EmployeeRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace controltime.Common.Models
+{
+    internal class EmployeeRecord
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
